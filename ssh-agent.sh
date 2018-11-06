0d4bae9a4655308a56ec01ad5b88e33aa2233dd8
Phil Dufault <phil@dufault.info>
2018-11-06 10:47:15 -0800

    Update ssh-agent.sh

--- ssh-agent.sh
+++ ssh-agent.sh
@@ -5,18 +5,19 @@ export AGENTALIVE=0
 
 # Start an ssh-agent and write out the details to $SSH_ENV
 function agent_start() {
-  echo -n " [i] Initializing a new SSH agent..."
+  # Check to see if this is an interactive session. If so then echo stuff. If not, don't.
+  if [[ $- =~ "i" ]]; then
+    echo -n " [i] Initializing a new SSH agent..."
+  fi
+  umask 177
   /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-  echo -n "$SSH_ENV "
-  chmod 600 "${SSH_ENV}"
   source "${SSH_ENV}" > /dev/null
-  echo
+  echo done
 }
 
 # Reconnect to the last ssh-agent that was loaded
 function agent_restore_or_start() {
-  if [[ -f $SSH_ENV ]]
-  then
+  if [[ -f $SSH_ENV ]]; then
     source $SSH_ENV
     agent_check_alive
   else
@@ -29,37 +30,33 @@ function agent_check_alive() {
   # Check to see if there are any ssh-agents running on the PID
   # that we loaded from the agent file.
   if [[ $SSH_AGENT_PID ]]; then
-    AGENTALIVE=$(ps wwaux | grep [s]sh-agent |grep -c $SSH_AGENT_PID)
+    AGENTALIVE=$(ps wwaux | grep [s]sh-agent | grep -c $SSH_AGENT_PID)
     if [[ $AGENTALIVE -ne 1 ]]
     then
-      # Check to see if this is an interactive session. If so then echo stuff. If not, don't.
-      if [[ $- =~ "i" ]]
-      then
-        start_agent
-        AGENTALIVE=$(ps wwaux | grep [s]sh-agent |grep -c $SSH_AGENT_PID)
-      fi
+      start_agent
+      AGENTALIVE=$(ps wwaux | grep [s]sh-agent | grep -c $SSH_AGENT_PID)
     fi
   else
-    AGENTALIVE=0
+      start_agent
+      AGENTALIVE=$(ps wwaux | grep [s]sh-agent | grep -c $SSH_AGENT_PID)
   fi
 
   # If the agent is alive, check to see if there are any loaded keys
   if [[ $AGENTALIVE -eq 1 ]]; then
-    # Check to see if this is an interactive session. If so then echo stuff. If not, don't.
-    if [[ $- =~ "i" ]]
-    then
-      no_key_loaded=$(ssh-add -l | grep -c "The agent has no identities.")
-      if [[ $no_key_loaded -eq 1 ]]; then
-        echo " [!] no loaded ssh keys"
-        if [[ -e ~/.ssh/id_rsa ]] || [[ -e ~/.ssh/id_ed25519 ]]; then
-          ssh-add
-        fi
-      else
-      # If you suffix your keys with _rsa or _dsa and want that trimmed out, use this line
-      # var=$(ssh-add -l | awk '{print $3}' | sed -e 's,.*/,,g' -e 's,_rsa,,g' -e 's,_dsa,,g' | xargs)
-      # Otherwise use this line
+    no_key_loaded=$(ssh-add -l | grep -c "The agent has no identities.")
+    if [[ $no_key_loaded -eq 1 ]]; then
+      # Check to see if this is an interactive session. If so then echo stuff. If not, don't.
+      if [[ $- =~ "i" ]]; then
+        echo " [!] No loaded SSH keys"
+      fi
+      if [[ -e ~/.ssh/id_rsa ]] || [[ -e ~/.ssh/id_ed25519 ]]; then
+        ssh-add
+      fi
+    else
+      # Check to see if this is an interactive session. If so then echo stuff. If not, don't.
+      if [[ $- =~ "i" ]]; then
         var=$(ssh-add -l | awk '{print $3}' | sed -e 's,.*/,,g' | xargs)
-        echo " [x] loaded ssh keys: $var"
+        echo " [x] Loaded ssh keys: $var"
       fi
     fi
   fi
